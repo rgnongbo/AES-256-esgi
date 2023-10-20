6c3c2f4e4500ebf3abcec74947982123d3e90b8e
Romain Locatelli <rlocatelli@myges.fr>
2023-10-20 19:31:33 +0200

    Modification des librairies

---
+++ aes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def encrypt(cle,txt):
+  sel = os.urandom(16)
+  cles = os.urandom(32)
+  key = pbkdf2_hmac(',sha3_512',cles, sel, 100000, 32) 
+  iv = os.urandom(AES.block_size)
+  cipher = AES.new(key, AES.MODE_GCM, iv) 
+  crypt_bytes = cipher.encrypt(pad(txt.encode(), AES.block_size))
+  encrypted_text = b64encode(crypt_bytes).decode('utf-8)
+  return encrypted_text, key.hex(), iv.hex()
