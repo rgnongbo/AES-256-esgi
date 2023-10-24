d97334b930d041900bec93bbf85fe0809481d954
Romain Locatelli <rlocatelli@myges.fr>
2023-10-24 19:39:49 +0200

    Ajout de la variable decrypted_text

--- fichier.py
+++ fichier.py
@@ -21,3 +21,4 @@ def decrypt(key, encrypted_text, iv):
     cipher = AES.new(key, AES.MODE_GCM, iv)
     encrypted_bytes = b64decode(encrypted_text)
     decrypted_bytes = cipher.decrypt(encrypted_bytes)
+    decrypted_text = unpad(decrypted_bytes, AES.block_size).decode('utf-8')
